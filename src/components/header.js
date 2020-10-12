01368fc9147231bdb91194d30e41616a617202ff
Dave Gaskin <davidburkegaskin@gmail.com>
2020-10-12 12:07:11 -0700

    add fixed header on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby"
 
 import Nav from "./nav"
 import Hamburger from "./theme/hamburger_menu"
 import siteLogo from "../images/bristol_hair_logo.png";
 
+import "./header.scss"
+
 export default function Header() {
+
     // Declare a new state variable, which we'll call "count"
     const [isOpen, toggleIsOpen] = useState(false);
+    const [isFloatingHeader, toggleFloatingHeader] = useState(false)
+
+    useEffect(() => {
+        console.log(window.scrollY);
+        const testScroll = () => {
+            const sticky = (window.scrollY > 200) ? 
+            toggleFloatingHeader(true)  : 
+            toggleFloatingHeader(false)
+        }
+        window.addEventListener('scroll', testScroll)
+    }, [isFloatingHeader])
 
     return (
-        <header id={"main_header"}>
+        <header id={"main_header"} className={(isFloatingHeader) ? "floating_header" : ""}>
+            <span className="hidden" id="top"></span>
             <div className={"section_inner"}>
                 <div className={"flex_inner"}>
                     <Link to="/">
