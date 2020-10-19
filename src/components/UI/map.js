327d56e6bad3d2faef85f6e977c20df4776958d3
Dave Gaskin <davidburkegaskin@gmail.com>
2020-10-19 14:32:52 -0700

    env experiment

--- src/components/UI/map.js
+++ src/components/UI/map.js
@@ -16,7 +16,7 @@ export default function MapBox() {
             }
         }
     `)
-    const googleKey = { key: `${process.env.GATSBY_GoogleMapKey}` }
+    const googleKey = { key: process.env.GATSBY_GoogleMapKey }
     const location = {
         address: '2562 Sinclair Rd, Victoria, BC V8N 1B8',
         lat: 48.461470,
@@ -26,7 +26,7 @@ export default function MapBox() {
     // console.log(siteMetadata);
 
     const handleApiLoaded = (map, maps) => {
-        console.log(map, maps);
+        // console.log(map, maps);
         let marker = new maps.Marker({
             position: location,
             map,
